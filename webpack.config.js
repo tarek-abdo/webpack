76043a7c3c7bcd9b5210de5721a307519ce3270a
tarek-abdo <tabdelnaby06@gmail.com>
2023-09-13 17:30:51 +0300

    project files

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const pathModule = require("path")
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: pathModule.join(__dirname, "build"),
+        assetModuleFilename: 'images/[name][ext]'
+    }
+    , mode: "production"
+    , module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  MiniCssExtractPlugin.loader,
+                  "css-loader",
+                  "sass-loader",
+                ],
+              },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({ template: "src/index.html", inject: "body" }),
+        new MiniCssExtractPlugin({filename:"style.min.css"})
+    ],
+    optimization: {
+      minimizer: [
+        `...`,
+        new CssMinimizerPlugin()
+        ,
+        new ImageMinimizerPlugin({
+            minimizer: {
+              implementation: ImageMinimizerPlugin.sharpMinify,
+              options: {
+                encodeOptions: {
+                },
+              },
+            },
+          }),
+      ],
+    },
+}
